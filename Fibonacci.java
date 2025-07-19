128b5a70c9f22e6171d0384375f511f6535c19a6
JessiJoya <jespjp1551@gmail.com>
2025-07-18 20:13:40 -0500

    Se creo el programa

---
+++ Fibonacci.java
@@ -0,0 +1,12 @@
+public class Fibonacci{
+	public static int Fibo (int a){
+		if (a>2){
+		return Fibo(a-1)+(a-2);
+		}else{
+		return 1;
+		}
+	}
+	public static void main (String[] args){
+        	System.out.println("Fibonacci es: "+Fibo(3));
+	}
+ }
