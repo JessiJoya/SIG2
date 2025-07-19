9ccc3141fe79dd24f08eb10d9c347545c1ce2b6f
JessiJoya <jespjp1551@gmail.com>
2025-07-18 20:21:42 -0500

    Se creo el programa

--- Fibonacci.java
+++ Fibonacci.java
@@ -7,6 +7,6 @@ public class Fibonacci{
 		}
 	}
 	public static void main (String[] args){
-        	System.out.println("Fibonacci es: "+Fibo(3));
+        	System.out.println("Fibonacci es: "+Fibo(5));
 	}
  }
